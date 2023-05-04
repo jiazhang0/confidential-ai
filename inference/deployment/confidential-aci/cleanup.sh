e823437568086857761ee036a48a0f443c21f0ab
Jia Zhang <zhang.jia@linux.alibaba.com>
2023-05-04 15:53:13 +0800

    Remove the temporary files
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- inference/deployment/confidential-aci/cleanup.sh
+++ inference/deployment/confidential-aci/cleanup.sh
@@ -2,3 +2,13 @@
 # Licensed under the MIT License.
 
 az group delete --yes --name $AZURE_RESOURCE_GROUP
+
+cd /tmp
+rm -f \
+  aci-parameters.json \
+  cce-policy.b64 \
+  importkey-config.json \
+  maa-parameters.json \
+  maa-policy.in \
+  policy-in.json
+cd -
