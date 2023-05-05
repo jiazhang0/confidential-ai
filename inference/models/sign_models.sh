9d857c8c5032d4d26bb18ab3321ea1627359169f
Jia Zhang <zhang.jia@linux.alibaba.com>
2023-05-05 13:32:14 +0800

    Reuse existing signing key for model signing
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- inference/models/sign_models.sh
+++ inference/models/sign_models.sh
@@ -4,8 +4,10 @@
 # Licensed under the MIT License.
 
 # Generate signing keys
-openssl ecparam -genkey -name secp384r1 -noout -out signing_private.pem
-openssl ec -in signing_private.pem -pubout -out signing_public.pem
+[[ ! -s signing_private.pem ]] && \
+  openssl ecparam -genkey -name secp384r1 -noout -out signing_private.pem
+[[ ! -s signing_public.pem ]] && \
+  openssl ec -in signing_private.pem -pubout -out signing_public.pem
 
 # Sign models
 python3 sign.py signing_private.pem model_repository
